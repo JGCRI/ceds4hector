c4ca1dc527a6a0c082a38942f0dfb3f2e1c3d3a1
Kalyn R. Dorheim <kalyn.dorheim@pnnl.gov>
2024-11-12 20:46:29 -0500

    Combine RCMIP and CEDS emissions for Hector (#4)
    Add the materials, scripts and mapping files that can be used to combine CEDS and RCMIP emissions to create Hector historical inputs.

--- scripts/constants.R
+++ scripts/constants.R
@@ -11,7 +11,14 @@ library(hector)
 # TODO probably use a package manager but for now this is probably good enough
 stopifnot(packageVersion("dplyr") == "1.1.4")
 stopifnot(packageVersion("tidyr") == "1.3.1")
+<<<<<<< HEAD
+stopifnot(packageVersion("here") == "1.0.1")
+stopifnot(packageVersion("zoo") == "1.8.12")
 
+zoo
+=======
+
+>>>>>>> main
 # packages that are probably not going to be required but could be helpful during
 # the developmental stage.
 library(ggplot2)
@@ -25,6 +32,54 @@ DIRS <- list(DATA = file.path(BASE, "data"),
              L1 = file.path(BASE, "data", "L1"),
              TABLES = file.path(BASE, "inputs", "tables"))
 
+<<<<<<< HEAD
+sapply(DIRS, dir.create, showWarnings = FALSE, recursive = TRUE)
+
+
+# # 2. Define Constants ----------------------------------------------------------
+#
+# # All of the CEDS species, these are the emissions that will have to be blended
+# # with the RCMIP species
+# CEDS_EMISS <- c("BC", "CH4", "CO", "CO2", "N2O", "NH3", "NMVOC", "NOx", "OC", "SO2")
+#
+# # This is the final year of the the CEDS data!
+# CEDS_FINAL_YEAR <- 2022
+#
+#
+#
+# # 3. Common Functions ----------------------------------------------------------
+#
+# # TODO should generalize to make more useful
+# # Extend the emissions until 1745
+# # Args
+# #   x: data.frame emissions that start in 1750
+# # Return: data.frame that has extended the emissions until 1745
+# extend_to_1745 <- function(x){
+#
+#     new_start_yr <- 1745
+#     to_copy <- filter(x, year == 1750)
+#
+#     scn <- rep(x = to_copy$scenario, each = 5)
+#     yr <-  rep(1745:1749, length.out = length(scn))
+#     val <- rep(x = to_copy$value, each = 5)
+#     var <- rep(x = to_copy$variable, each = 5)
+#     u <- rep(x = to_copy$units, each = 5)
+#
+#     data.frame(scenario = scn,
+#                year = yr,
+#                value = val,
+#                variable = var,
+#                units = u) ->
+#         early_eimss
+#
+#     rbind(x, early_eimss) %>%
+#         arrange(scenario, variable, year) ->
+#         out
+#
+#     return(out)
+#
+# }
+=======
 dir.create(DIRS$L0, showWarnings = FALSE)
 dir.create(DIRS$L1, showWarnings = FALSE)
 
@@ -81,3 +136,4 @@ extend_to_1745 <- function(x){
 
 
 
+>>>>>>> main
