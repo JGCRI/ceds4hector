4f38b47abb39d8351522711cd7c27344a6488c6d
Kalyn R. Dorheim <kalyn.dorheim@pnnl.gov>
2025-03-27 17:10:31 -0400

    run hector (#12)
    Run hector

--- scripts/L3A.run_hector.R
+++ scripts/L3A.run_hector.R
@@ -10,20 +10,21 @@ source(here::here("scripts", "constants.R"))
 # Save a copy of all the variables, don't include the tracking data or any 
 # constraints since hector won't be run with constraints, or is a constant 
 # parameter. 
-params <- c(hector::inputstable$parameter[hector::inputstable$time.variant  == "n"], "lo_warming_ratio")
-all_vars <- hector::fxntable$string
-all_vars <- all_vars[!grepl(pattern = "tracking|constrain|biome|warmingfactor", x = tolower(all_vars))]
-all_vars <- c(setdiff(all_vars, c(params, "FCH4")), "RF_CH4")
+vars_to_save <- c("BC_emissions", "CH4_emissions", "CO_emissions", "ffi_emissions", 
+                  "N2O_emissions", "NH3_emissions", "NMVOC_emissions", "NOX_emissions", 
+                  "OC_emissions", "SO2_emissions", "RF_tot", "RF_CO2", 
+                  "RF_N2O", "RF_H2O_strat", "RF_O3_trop", "RF_BC", "RF_OC", 
+                  "RF_NH3", "RF_SO2", "RF_aci", "FCH4", "global_tas", "O3_concentration", 
+                  "CO2_concentration", "N2O_concentration", "CH4_concentration")
 
-yrs <- 1750:2023
 
 
 # 1. Run CMIP6 Historical ------------------------------------------------------
 # Run Hector with SSP2-45 scenario as the CMIP6 historical pathway. 
 ini <- system.file(package = "hector", "input/hector_ssp245.ini")
 hc <- newcore(ini)
-run(hc, runtodate = 2023)
-out1 <- fetchvars(hc, yrs, all_vars)
+run(hc, runtodate = 2015)
+out1 <- fetchvars(hc, 1750:2015, vars_to_save)
 out1$scenario <- "CMIP6 CEDS"
 
 
@@ -32,51 +33,45 @@ out1$scenario <- "CMIP6 CEDS"
 ini <- file.path("inputs", "hector_ceds_historical.ini")
 hc <- newcore(ini)
 run(hc, runtodate = 2023)
-out2 <- fetchvars(hc, yrs, all_vars)
+out2 <- fetchvars(hc, 1750:2023, vars_to_save)
 out2$scenario <- "CMIP7 CEDS"
 
 
 # 3. Calculate the differences between the output ------------------------------
 
-difference_df <- out1
-difference_df$value <- out2$value - out1$value
-difference_df$scenario <- "CMIP7 CEDS - CMIP6 CEDS"
+# Combine into a data frame. 
+out <- rbind(out1, out2)
+
+out %>% 
+  filter(variable %in% c(RF_ACI(), RF_BC(), RF_OC(), RF_SO2(), RF_NH3())) %>% 
+  summarise(value = sum(value), .by = c("scenario", "year", "units")) %>% 
+  mutate(variable = "total aerosol RF") -> 
+  total_aerosol_rf
+
+out %>% 
+  rbind(total_aerosol_rf) -> 
+  out
+
+out %>% 
+  select(scenario, year, variable, value) %>% 
+  spread(scenario, value) %>% 
+  na.omit %>% 
+  mutate(difference = `CMIP7 CEDS` - `CMIP6 CEDS`) -> 
+  diff_out
+  
+
+# Some summary results 
+diff_out %>% 
+  filter(variable %in% c(GLOBAL_TAS(), RF_TOTAL(), "total aerosol RF")) %>% 
+  summarise(mean_diff = sqrt(mean(difference^2)), .by = c("variable")) %>% 
+  knitr::kable()
 
 
 # 4. Save results for all variables --------------------------------------------
 # Save the results for all of the hector variables 
-write.csv(out1, file = file.path(DIRS$L3, "hector_CMIP6_CEDS_all_data.csv"), row.names = FALSE)
-write.csv(out2, file = file.path(DIRS$L3, "hector_CMIP7_CEDS_all_data.csv"), row.names = FALSE)
-write.csv(difference_df, file = file.path(DIRS$L3, "hector_difference_CEDS_all_data.csv"), row.names = FALSE)
+write.csv(out, file = file.path(DIRS$L3, "hector_CEDS_rslts_data.csv"), row.names = FALSE)
+write.csv(diff_out, file = file.path(DIRS$L3, "hector_difference.csv"), row.names = FALSE)
 
-
-# 5. Identify the variables of interest ----------------------------------------
-
-# How we will define 0 in terms of sqrt(MSE)
-zero_def <- 1e-9
-
-# Determine which variables have a difference in the MSE. 
-difference_df %>% 
-  summarise(dif = sqrt(mean(value^2)), .by = c(variable)) %>% 
-  filter(dif > zero_def) %>% 
-  pull(variable) -> 
-  vars_to_keep
-
-
-# Subset the data sets so that they only include variables that contain different 
-# results. 
-out1 %>% 
-  rbind(out2) %>% 
-  filter(variable %in% vars_to_keep) -> 
-  raw_hector_vars 
-
-difference_df %>% 
-  filter(variable %in% vars_to_keep) -> 
-  difference_in_hector_vars
-
-# Save the results for all of the hector variables 
-write.csv(raw_hector_vars, file = file.path(DIRS$L3, "select_hector_results.csv"), row.names = FALSE)
-write.csv(difference_in_hector_vars, file = file.path(DIRS$L3, "select_hector_difference_in_results.csv"), row.names = FALSE)
 
 
 # 6.Plot Results ---------------------------------------------------------------
@@ -96,16 +91,16 @@ FIG_WIDTH <- 10
 FIG_HEIGHT <- 8
 
 # Vector of variables to use in the plotting 
-vars_to_plot <- unique(difference_in_hector_vars$variable)
+vars_to_plot <- unique(diff_out$variable)
 
 
 lapply(vars_to_plot, function(var){
   
-  difference_in_hector_vars %>% 
+  diff_out %>% 
     filter(variable == var) -> 
     to_plot1
   
-  raw_hector_vars %>% 
+  out %>% 
     filter(variable == var) -> 
     to_plot2
   
@@ -116,7 +111,7 @@ lapply(vars_to_plot, function(var){
   # Plot the difference in the variable 
   to_plot1 %>% 
     #filter(year <= 2014) %>% 
-    ggplot(aes(year, value)) + 
+    ggplot(aes(year, difference)) + 
     geom_line() + 
     labs(subtitle = "CMIP7 CEDS - CMIP6 CEDS", 
          y = units, 
@@ -129,10 +124,10 @@ lapply(vars_to_plot, function(var){
   to_plot2 %>% 
     ggplot(aes(year, value, color = scenario)) + 
     geom_line() + 
-    labs(subtitle = "CMIP7 CEDS - CMIP6 CEDS", 
-         y = units, 
+    labs(y = units, 
          title = nice_var, 
-         x = NULL) -> 
+         x = NULL) + 
+    scale_color_manual(values = c("CMIP6 CEDS" = "grey", "CMIP7 CEDS" = "red")) -> 
     plot2
   
   ggsave(filename = file.path(FIG_DIR_DIFF, paste0("difference_in", var, ".png")),
