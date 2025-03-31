ecb69fa7c8822683f9b857d8034e5c4de6bf9bb4
Kalyn R. Dorheim <kalyn.dorheim@pnnl.gov>
2025-03-31 13:50:52 -0400

    Run SSP emissions out until 2023 (#13)

--- scripts/L3A.run_hector.R
+++ scripts/L3A.run_hector.R
@@ -22,10 +22,26 @@ vars_to_save <- c("BC_emissions", "CH4_emissions", "CO_emissions", "ffi_emission
 # 1. Run CMIP6 Historical ------------------------------------------------------
 # Run Hector with SSP2-45 scenario as the CMIP6 historical pathway. 
 ini <- system.file(package = "hector", "input/hector_ssp245.ini")
-hc <- newcore(ini)
-run(hc, runtodate = 2015)
-out1 <- fetchvars(hc, 1750:2015, vars_to_save)
-out1$scenario <- "CMIP6 CEDS"
+
+all_ssp_inis <- list.files(system.file(package = "hector", "input"), 
+                       full.names = TRUE, pattern = "ssp.{,3}ini" )
+
+lapply(all_ssp_inis, function(ini){
+  
+  ssp_name <- gsub(basename(ini), pattern = "hector_|.ini", replace = "")
+  
+  hc <- newcore(ini)
+  run(hc, runtodate = 2023)
+  out <- fetchvars(hc, 1750:2023, vars_to_save)
+  out$scenario <- "CMIP6 CEDS"  
+  out$ssp <- ssp_name
+  return(out)
+  
+}) %>% 
+  do.call(what = "rbind") -> 
+  out1
+
+
 
 
 # 2. Run CMIP6 Historical ------------------------------------------------------
@@ -40,11 +56,11 @@ out2$scenario <- "CMIP7 CEDS"
 # 3. Calculate the differences between the output ------------------------------
 
 # Combine into a data frame. 
-out <- rbind(out1, out2)
+out <- bind_rows(out1, out2)
 
 out %>% 
   filter(variable %in% c(RF_ACI(), RF_BC(), RF_OC(), RF_SO2(), RF_NH3())) %>% 
-  summarise(value = sum(value), .by = c("scenario", "year", "units")) %>% 
+  summarise(value = sum(value), .by = c("scenario", "year", "units", "ssp")) %>% 
   mutate(variable = "total aerosol RF") -> 
   total_aerosol_rf
 
@@ -53,6 +69,7 @@ out %>%
   out
 
 out %>% 
+  filter(ssp %in% c("ssp245", NA)) %>% 
   select(scenario, year, variable, value) %>% 
   spread(scenario, value) %>% 
   na.omit %>% 
@@ -71,7 +88,6 @@ diff_out %>%
 # Save the results for all of the hector variables 
 write.csv(out, file = file.path(DIRS$L3, "hector_CEDS_rslts_data.csv"), row.names = FALSE)
 write.csv(diff_out, file = file.path(DIRS$L3, "hector_difference.csv"), row.names = FALSE)
-
 
 
 # 6.Plot Results ---------------------------------------------------------------
@@ -110,7 +126,7 @@ lapply(vars_to_plot, function(var){
   
   # Plot the difference in the variable 
   to_plot1 %>% 
-    #filter(year <= 2014) %>% 
+    filter(year <= 2014) %>% 
     ggplot(aes(year, difference)) + 
     geom_line() + 
     labs(subtitle = "CMIP7 CEDS - CMIP6 CEDS", 
@@ -122,7 +138,7 @@ lapply(vars_to_plot, function(var){
   
   # Plot the raw results 
   to_plot2 %>% 
-    ggplot(aes(year, value, color = scenario)) + 
+    ggplot(aes(year, value, color = scenario, by = ssp)) + 
     geom_line() + 
     labs(y = units, 
          title = nice_var, 
@@ -130,7 +146,7 @@ lapply(vars_to_plot, function(var){
     scale_color_manual(values = c("CMIP6 CEDS" = "grey", "CMIP7 CEDS" = "red")) -> 
     plot2
   
-  ggsave(filename = file.path(FIG_DIR_DIFF, paste0("difference_in", var, ".png")),
+  ggsave(filename = file.path(FIG_DIR_DIFF, paste0("difference_in_", var, ".png")),
          plot = plot1, width = FIG_WIDTH , height = FIG_HEIGHT)
   
   ggsave(filename = file.path(FIG_DIR, paste0(var, ".png")),
