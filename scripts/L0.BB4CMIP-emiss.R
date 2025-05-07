12901cb47e56a0ee5ce71ec967ba840215c9c96b
kdorheim <kalyn.dorheim@pnnl.gov>
2025-05-06 22:55:51 -0400

    final touches

--- scripts/L0.BB4CMIP-emiss.R
+++ scripts/L0.BB4CMIP-emiss.R
@@ -4,16 +4,14 @@
 #
 # The time series were extended to 2023 by repeating the mean 2012:2022 emissions.
 
-# TODO need to add the CMIP7 ciatation here
-# 
 # Data Source: van Marle, M. J. E., Kloster, S., Magi, B. I., Marlon, J. R., 
 # Daniau, A.-L., Field, R. D., Arneth, A., Forrest, M., Hantson, S., Kehrwald, 
 # N. M., Knorr, W., Lasslop, G., Li, F., Mangeon, S., Yue, C., Kaiser, J. W., 
 # and van der Werf, G. R.: Historic global biomass burning emissions for CMIP6 
-# (BB4CMIP) based on merging satellite observations with proxies and fire models (1750–2015), Geosci. Model Dev., 10, 3329-3357, https://doi.org/10.5194/gmd-10-3329-2017, 2017.
-# Data extracted by country using tiled, gridded country masks as used in CEDS:
-#  http://www.globalchange.umd.edu/ceds/
-#  
+# (BB4CMIP) based on merging satellite observations with proxies and fire models (1750–2015), 
+# Geosci. Model Dev., 10, 3329-3357, https://doi.org/10.5194/gmd-10-3329-2017, 2017.
+# ** updated for CMIP7 see https://input4mips-cvs.readthedocs.io/en/latest/dataset-overviews/open-biomass-burning-emissions/
+
 
 
 # 0. Set Up --------------------------------------------------------------------
